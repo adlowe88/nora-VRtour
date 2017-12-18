17f2744f86e4a8babfa8475b640d9f6eba094c96
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-18 11:12:46 +1100

    Tests

--- Nora-LiveTour/index.vr.js
+++ Nora-LiveTour/index.vr.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+// import React from 'react';
+// import {
+//   AppRegistry,
+//   asset,
+//   Pano,
+//   Text,
+//   View,
+// } from 'react-vr';
+//
+// export default class Nora_LiveTour extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <Pano source={asset('IMG_0440.JPG')}/>
+//         <Text
+//           style={{
+//             backgroundColor: '#777879',
+//             fontSize: 0.8,
+//             fontWeight: '400',
+//             layoutOrigin: [0.5, 0.5],
+//             paddingLeft: 0.2,
+//             paddingRight: 0.2,
+//             textAlign: 'center',
+//             textAlignVertical: 'center',
+//             transform: [{translate: [0, 0, -3]}],
+//           }}>
+//           Good Morning Ezra!
+//         </Text>
+//       </View>
+//       <Test />
+//     );
+//   }
+// };
+//
+// AppRegistry.registerComponent('Nora_LiveTour', () => Nora_LiveTour);
 
-export default class Nora_LiveTour extends React.Component {
+import React from 'react';
+import { AppRegistry } from 'react-vr';
+import { LiveTour } from 'live-tour-lab';
+import Test from './Test';
+
+export default class MyLiveTour extends React.Component {
   render() {
     return (
-      <View>
-        <Pano source={asset('IMG_0440.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          Good Morning Ezra!
-        </Text>
-      </View>
+      <LiveTour entries="test" tourURI="Text.json"/>
     );
   }
 };
 
-AppRegistry.registerComponent('Nora_LiveTour', () => Nora_LiveTour);
+AppRegistry.registerComponent('MyLiveTour', () => MyLiveTour);
