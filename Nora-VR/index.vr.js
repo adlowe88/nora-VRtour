7632a396c27cf52f48ebd97395ed2aab9adb5a4e
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-19 15:44:02 +1100

    indexvr

--- Nora-VR/index.vr.js
+++ Nora-VR/index.vr.js
@@ -7,25 +7,63 @@ import {
   View,
 } from 'react-vr';
 
+//max possible for rendering in web pages
+const MAX_TEXTURE_WIDTH = 4096;
+const MAX_TEXTURE_HEIGHT = 720;
+
+// pixels = degrees/360 * density, negative to rotate in expected direction.
+const degreesToPixels = degrees => -(degrees / 360) * MAX_TEXTURE_WIDTH;
+// PPM = 1/(2*PI*Radius) * density. Radius of cylinder is 3 meters.
+const PPM = 1 / (2 * Math.PI * 3) * MAX_TEXTURE_WIDTH;
+
 export default class Nora_VR extends React.Component {
+  //for underfined props (but not null)
+  static defaultProps = {
+    src: 'equi1.jpg',
+  };
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: null,
+      locationId: null,
+      nextLocationId: null,
+      rotation: null,
+    };
+  }
+
+  //invoked immediately after component mount
+  //Starting AJAX calls to load in data for your component.
+  //load data from remoate endpoint, can't guarentee AJAX requests will resolve
+  //before component mounts; guarentee that component is up to date before trying to setState
+  componentDidMount() {
+    fetch(asset(this.props.src).uri)
+      .then(response => response.json())
+      .then(responseData => {
+        this.init(responseData);
+      })
+      .done();
+  }
+
+  //Initialize the tour with the data config file
+  init(config) {
+    this.setState({
+      data: config,
+      locationId: null,
+      nextLocationId: config.firstPhotoId,
+      rotation: config.firstPhotoRotation +
+        ( config.photos[config.firstPhotoId].rotationOffSet || 0 ),
+    });
+  }
+
   render() {
+    if(!this.state.data) {
+      return null;
+    }
+
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+
       </View>
     );
   }
