a797cad04296091d47a54f92bd2e1988e90ed575
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-19 18:00:28 +1100

    finished index component

--- Nora-VR/index.vr.js
+++ Nora-VR/index.vr.js
@@ -60,10 +60,91 @@ export default class Nora_VR extends React.Component {
     if(!this.state.data) {
       return null;
     }
+    //Set start scene
+    const locationId = this.state.locationId;
+    //image info for starting scene
+    const photoData = (locationId && this.state.data.photos[locationId]) || null;
+    //nav buttons, tooltips
+    const tooltips = (photoData && photoData.tooltips) || null;
+    const rotation =
+      this.state.data.firstPhotoRotation +
+        ((photoData && photoData.rotationOffset) || 0);
+    //need to load next scene?
+    const isLoading = this.state.nextLocationId !== this.state.locationId;
 
     return (
       <View>
-
+        <View style={{transform: [{rotateY: rotation}]}}>
+          <Pano
+            //absoulte to overcome re-positioning within the flexbox style layout
+            style={{
+              position: 'absolute',
+              tintColor: isLoading ? 'grey' : 'white',
+            }}
+            onLoad={() => {
+              const data = this.state.data;
+              this.setState({
+                //update locationId
+                locationId: this.state.nextLocationId,
+              });
+            }}
+            //set the source of the new scene
+            source={asset(this.state.data.photos[this.state.nextLocationId].uri)}
+          />
+          //To draw the child components to the inner surface of a cylinder
+          //rendering flat 2d content in a 3d space
+          <CylindricalPanel
+            layer={{
+              width: MAX_TEXTURE_WIDTH,
+              height: MAX_TEXTURE_HEIGHT,
+              density: MAX_TEXTURE_WIDTH,
+            }}
+            style={{position: 'absolute'}}>
+            <View
+              style={{
+                // View covering the cyldiner. Center so contents appear in middle of cylinder.
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: MAX_TEXTURE_WIDTH,
+                height: MAX_TEXTURE_HEIGHT,
+              }}>
+              {/* Need container view, else using absolute position on buttons removes them from cylinder */}
+              <View>
+                {tooltips &&
+                  tooltips.map((tooltip, index) => {
+                    // Iterate through items related to this location, creating  nav buttons which
+                    // change the current location in the tour.
+                    return (
+                      <NavButton
+                        key={tooltip.linkedPhotoId}
+                        isLoading={isLoading}
+                        onInput={() => {
+                          // Update nextLocationId, not locationId, so tooltips match
+                          // the currently visible pano; pano will update locationId
+                          // after loading the new image.
+                          this.setState({
+                            nextLocationId: tooltip.linkedPhotoId,
+                          });
+                        }}
+                        pixelsPerMeter={PPM}
+                        source={asset(this.state.data.nav_icon)}
+                        textLabel={tooltip.text}
+                        translateX={degreesToPixels(tooltip.rotationY)}
+                      />
+                    );
+                  })}
+                {locationId == null &&
+                  // Show a spinner while first pano is loading.
+                  <LoadingSpinner
+                    style={{layoutOrigin: [0.5, 0.5]}}
+                    pixelsPerMeter={PPM}
+                    // Undo the rotation so spinner is centered
+                    translateX={degreesToPixels(rotation) * -1}
+                  />}
+              </View>
+            </View>
+          </CylindricalPanel>
+        </View>
       </View>
     );
   }
